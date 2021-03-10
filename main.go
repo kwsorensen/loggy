0666f7dae4f42e98e9cd0bacfc5a6f2d76a25f83
ksorensen@scramsystems.com <ksorensen@scramsystems.com>
2021-03-10 11:32:36 -0700

    loggingg added

--- main.go
+++ main.go
@@ -1,22 +1,39 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
 	"os"
+	"time"
 )
 
-func setupLogger() {
+func startLogging() {
+	counter := 0
+	for {
+		time.Sleep(time.Second * 5)
+		log.Println("Logging Line: ", counter, "Line")
+		counter++
+	}
+}
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
 func main() {
 	// If the file doesn't exist, create it or append to the file
-	os.MkdirAll("/mnt/c/logs/app1/", 0666)
-	file, err := os.OpenFile("/mnt/c/logs/app1/logs/logs.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	os.MkdirAll("/mnt/c/logs/app2/logs", 0666)
+	file, err := os.OpenFile("/mnt/c/logs/app2/logs/logs.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	log.SetOutput(file)
-	log.Println("Hello world!")
+	log.Println("Log Generator Starting!")
+
+	go startLogging()
+
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
