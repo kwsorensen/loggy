d006747db6406fbbc3edf8a7fd6ebb596e5f1661
kyle <sorensenkw@gmail.com>
2021-03-28 00:21:17 -0600

    rough draft of an api

---
+++ loggio/loggio.go
@@ -0,0 +1,46 @@
+package loggio
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"time"
+)
+
+type JsonLog struct {
+	Count   int32  `json:"count"`
+	Message string `json:"message"`
+}
+
+type LogType struct {
+	LogType  string `json:"string"`
+	Periodms int32  `json:"period_ms"`
+	Message  string `json:"message"`
+}
+
+func CreateJSONLog() {
+	logCount := 0
+	logObject := JsonLog{}
+	logObject.Message = "Hello string"
+	for {
+		logObject.Count = int32(logCount)
+		time.Sleep(time.Second * 5)
+		item, err := json.Marshal(logObject)
+		if err != nil {
+			log.Fatalln("Fatal Error:", err)
+		}
+		fmt.Println(string(item))
+
+		logCount++
+	}
+}
+
+func CreateDefaultLog() {
+	logCount := 0
+
+	for {
+		time.Sleep(time.Second * 5)
+		fmt.Println("New Log Number: ", logCount)
+		logCount++
+	}
+}
