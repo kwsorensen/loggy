15a3bbe773cf7d426d1264760662e1d4cf88c435
kyle <sorensenkw@gmail.com>
2021-04-02 23:46:20 -0600

    code logic added back after mistake

--- loggio/loggio.go
+++ loggio/loggio.go
@@ -19,14 +19,14 @@ type LogType struct {
 	Message  string `json:"message"`
 }
 
-func CreateJSONLog() {
+func CreateJSONLog(interval int) {
 	logCount := 0
 	logObject := JsonLog{}
 	logObject.LogLevel = "Information"
 	logObject.Message = "Loggy is the greatest Logger out there."
 	for {
 		logObject.Count = int32(logCount)
-		time.Sleep(time.Second * 5)
+		time.Sleep(time.Millisecond * time.Duration(interval))
 		item, err := json.Marshal(logObject)
 		if err != nil {
 			log.Fatalln("Fatal Error:", err)
@@ -37,11 +37,11 @@ func CreateJSONLog() {
 	}
 }
 
-func CreateDefaultLog() {
+func CreateDefaultLog(interval int) {
 	logCount := 0
 
 	for {
-		time.Sleep(time.Second * 5)
+		time.Sleep(time.Millisecond * time.Duration(interval))
 		fmt.Println("New Log Number: ", logCount)
 		logCount++
 	}
