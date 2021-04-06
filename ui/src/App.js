aaba8df222ca42f3d34c8e986c4895a7aa9db9d6
kyle <sorensenkw@gmail.com>
2021-04-05 22:32:35 -0600

    basic state included in app

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,15 +1,44 @@
 // import logo from './logo.svg';
 import loggo from './loggy.PNG'
 import './App.css';
+import { Component } from 'react';
 
-function App() {
+
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      beginning: 5
+    }
+  }
+
+  componentDidMount() { // calls a function every one second
+    this.timerID = setInterval(
+      () => this.clickedButton(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  clickedButton() {
+    console.log("Button Clicked")
+    this.setState({
+      beginning: this.state.beginning + 1
+    })
+  }
+
+  render() {
   return (
     <div className="App">
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <img src={loggo} className='App-logo' alt='Loggo the Loggy Logo' />
         <p>
-          Logging shit one line at a time. Again.
+          Logging shit one line at a time.
         </p>
         <a
           className="App-link"
@@ -19,9 +48,15 @@ function App() {
         >
           Contribute to loggy!
         </a>
+        <button onClick={() =>this.clickedButton()}>
+          Activate Button {this.state.beginning}
+        </button>
       </header>
     </div>
   );
 }
+}
+
+
 
 export default App;
